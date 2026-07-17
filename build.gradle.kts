dd45aec1993502d19a71e2f7b96b1cf19dd34d08
Robert Söderbjörn <robert.soderbjorn@framna.com>
2026-07-17 17:05:46 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "se.soderbjorn.darkness"
-    version = "0.2.35"
+    version = "0.2.36"
 }
 
 // Default file-Maven-repo locations inside the consumer worktrees. Each
