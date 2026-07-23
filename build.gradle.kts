7409d360b634ec8a0ae8c8727cd51e2aa1628924
Robert Söderbjörn <robert.soderbjorn@framna.com>
2026-07-23 14:15:44 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "se.soderbjorn.lunula"
-    version = "0.2.48"
+    version = "0.2.49"
 }
 
 // Default file-Maven-repo locations inside the consumer worktrees. Each
