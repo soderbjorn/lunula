e189e5ed255760dbe86cccc304c2fd666b9a9b18
Robert Söderbjörn <robert.soderbjorn@framna.com>
2026-07-27 02:04:58 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "se.soderbjorn.lunula"
-    version = "0.2.60"
+    version = "0.2.61"
 }
 
 // The toolkit publishes to a single file-Maven-repo whose location is supplied
